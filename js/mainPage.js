2cd54e8b20a16c4362bf2b6c6f7965a92ac52504
svee5 <svee5@student.monash.edu>
2016-05-26 19:15:28 +0800

    Update mainPage.js

--- js/mainPage.js
+++ js/mainPage.js
@@ -3,10 +3,32 @@
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
 
-function viewLocation(locationName)
+        function viewLocation(locationName)
 {
     // Save the desired location to local storage
     localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
     // And load the view location page.
     location.href = 'viewlocation.html';
+}
+   
+var length = LocationWeatherCache.length() ; //should be constructor
+var info, var list = " ";
+
+var date = new Date() ; 
+date = date.forecastDateString(); 
+
+var listRef = document.getElementById("list");
+
+for (var i = 0 ; i<length;i++) { 
+info = LocationWeatherCache.locationAtIndex(i) ;
+var nickname = info.nickname ; 
+    
+    list += "<li class='mdl-list__item mdl-list__item--two-line' onclick='viewLocation("+(i+1)+");'>";
+    list += "<span class='mdl-list__item-primary content'>";
+    list += "<img class='mdl-list__item-icon' id='icon"+(i+1)+"' src='images/loading.png' class='list-avatar' />";
+    list += "<span id= 'nickname'>"+nickname+"</span>" 
+    list += "<span id='weather1" + (i+1)+"' class='mdl-list__item-sub-title'>Min: - , Max : - </span>";
+    list+= "</span>";
+    list+= "</li>"
+    
 }
