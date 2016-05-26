263a6836c56d1413967d6784527eb22cfd9ec708
svee5 <svee5@student.monash.edu>
2016-05-26 20:30:06 +0800

    Update mainPage.js

--- js/mainPage.js
+++ js/mainPage.js
@@ -29,6 +29,27 @@ var nickname = info.nickname ;
     list += "<span id= 'nickname'>"+nickname+"</span>" 
     list += "<span id='weather1" + (i+1)+"' class='mdl-list__item-sub-title'>Min: - , Max : - </span>";
     list+= "</span>";
-    list+= "</li>"
+    list+= "</li>";
+    
+    //Display the List 
+    listRef.innerHTML += list;
+    list = ' ';
     
 }
+//Get weather info
+for (var i = 0; i<length,i++){
+    LocationWeatherCache.getWeatherAtIndexForDate(i,date,Weather);
+}
+
+//Callback
+function displayWeather(index,info){
+    var min =  info.data[0].temperatureMin ; 
+    var max = info.data[0].temperatureMax ; 
+    var img = 'images/' + info.data[0].icon + '.png'
+
+var refTemp = document.getElementById('weather'+String(index+1));
+
+refTemp.innerHTML = 'Min:' + min + ', Max: ' + max;
+refImg.src = img;
+}
+
