ca61e314d46ef36bc32317d318f35224b0923290
arieltakom <altak2@student.monash.edu>
2016-05-29 20:57:06 +0800

    Update mainPage.js

--- js/mainPage.js
+++ js/mainPage.js
@@ -1,55 +1,40 @@
 // Code for the main app page (locations list).
 
-// This is sample code to demonstrate navigation.
-// You need not use it for final app.
+// Initialize the location list HTML output.
+var outputRef = document.getElementById("locationList");
+var listHTML = "";
 
-        function viewLocation(locationName)
+// Loops through each stored location and place it as a list in the HTML page, 
+// and find the weather information for that location.
+ for (var i = 0; i < locationsObject.length(); i++)
+	{
+		locationsObject.getWeatherAtIndexForDate(i, date, updateLocationList);
+		listHTML+= '<li class="mdl-list__item mdl-list__item--two-line" id="location' + i + '"' + '>'
+		listHTML+= '<span class="mdl-list__item-primary-content" onclick="viewLocation(' + i + ')"' + '>'
+		listHTML+= '<img class="mdl-list__item-icon" id="iconLocation' + i + '" src="images/loading.png" class="list-avatar" />'
+		listHTML+= '<span>' + locationsObject.locationAtIndex(i).nickname + '</span>'
+		listHTML+= '<span id="weather' + i + '"class="mdl-list__item-sub-title"> Loading </span>';
+		listHTML+= '</span>'
+		listHTML+= '</li>'
+		saveLocations()
+	}
+outputRef.innerHTML = listHTML;
+
+
+// Callback function to retrieve the weather condition of the locations.
+function updateLocationList(locationIndex, weatherObject)
+	{
+		var listHTML = document.getElementById('weather'+locationIndex);
+		var iconHTML = document.getElementById('iconLocation'+locationIndex);
+		listHTML.innerHTML = "Min: " + weatherObject.daily.data[0].apparentTemperatureMin + "\xB0C , Max : " + weatherObject.daily.data[0].apparentTemperatureMax + "\xB0C </span>";
+		iconHTML.src = "images/" + weatherObject.daily.data[0].icon + ".png";
+	}
+
+// Views the selected location.
+function viewLocation(locationName)
 {
     // Save the desired location to local storage
     localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
     // And load the view location page.
     location.href = 'viewlocation.html';
 }
-   
-var length = LocationWeatherCache.length() ; //should be constructor
-var info, var list = " ";
-
-var date = new Date() ; 
-date = date.forecastDateString(); 
-
-var listRef = document.getElementById("list");
-
-for (var i = 0 ; i<length;i++) { 
-info = LocationWeatherCache.locationAtIndex(i) ;
-var nickname = info.nickname ; 
-    
-    list += "<li class='mdl-list__item mdl-list__item--two-line' onclick='viewLocation("+(i+1)+");'>";
-    list += "<span class='mdl-list__item-primary content'>";
-    list += "<img class='mdl-list__item-icon' id='icon"+(i+1)+"' src='images/loading.png' class='list-avatar' />";
-    list += "<span id= 'nickname'>"+nickname+"</span>" 
-    list += "<span id='weather1" + (i+1)+"' class='mdl-list__item-sub-title'>Min: - , Max : - </span>";
-    list+= "</span>";
-    list+= "</li>";
-    
-    //Display the List 
-    listRef.innerHTML += list;
-    list = ' ';
-    
-}
-//Get weather info
-for (var i = 0; i<length,i++){
-    LocationWeatherCache.getWeatherAtIndexForDate(i,date,Weather);
-}
-
-//Callback
-function displayWeather(index,info){
-    var min =  info.data[0].temperatureMin ; 
-    var max = info.data[0].temperatureMax ; 
-    var img = 'images/' + info.data[0].icon + '.png'
-
-var refTemp = document.getElementById('weather'+String(index+1));
-
-refTemp.innerHTML = 'Min:' + min + ', Max: ' + max;
-refImg.src = img;
-}
-
